bf97e7fd2dd570fe7fb8da9144f6a1e2cd2e9022
fnnyrawnnn <fannyirawannn@gmail.com>
2023-02-27 17:20:20 +0700

    push tugas 2 stupen msib pt gits indonesia

---
+++ Tugas_2/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Fanny Irawan</title>
+
+     <link rel="stylesheet" href="assets/style/cv.css?v=<?php echo time(); ?>">
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+     <div class="container text-center">
+          <div class="row justify-content-center">
+               <div class="col-4">
+                    <img src="assets/image/mie.jpg" class="image" alt="">
+               </div>
+               <div class="col-4">
+                    <h2 class="name">Fanny Irawan</h2>
+                    <div class="para">
+                         <p>
+                              Hola! I am an Undergraduate Information System Student at Telkom University. I am a data and software development enthusiast. I am a fast learner, consistent, like to learn new things, and can adapt quickly to a new work environment. I have skills in data analysis and software development using tools like Excel, Data Studio, Python, Laravel, WordPress, etc. I also have several experiences such as being active in several organizations, teaching in laboratories as a Practicum Assistant, and winning several business idea competitions with my team both nationally and internationally.
+                         </p>
+                    </div>
+                    <div>
+                         <button type="button" class="btn btn-primary" onclick="salken()">Contact Me</button>
+                         <a href="resume.php"><button type="button" class="btn btn-primary">My Resume</button></a>
+                    </div>
+               </div>
+          </div>
+     </div>
+
+     <script src="assets/source/chat.js"></script>
+</body>
+</html>
